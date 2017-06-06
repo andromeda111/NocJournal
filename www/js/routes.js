0f54ecce1ae39ff9c8d1797f38e54e923e644f1f
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-06 12:37:36 -0700

    added linter and did a little linting

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,13 +1,11 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
-
+.config(function ($stateProvider, $urlRouterProvider) {
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-
 
   //     .state('menu.home', {
   //   url: '/page1',
@@ -99,5 +97,4 @@ angular.module('app.routes', [])
 
 $urlRouterProvider.otherwise('/side-menu21')
 
-
-});
+})
