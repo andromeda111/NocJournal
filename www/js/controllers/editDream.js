4c867ca385534920280cd2d14f34d592e969b354
Amalia Claire Fergin <acfergin@msn.com>
2017-06-07 16:14:35 -0700

    dates rendering in edit form

--- www/js/controllers/editDream.js
+++ www/js/controllers/editDream.js
@@ -6,27 +6,15 @@ angular.module('app.editDreams', [])
     const id = $stateParams.id
     const apiUrl = 'http://dream-frog.herokuapp.com'
 
-    $scope.updateDream = []
-    $scope.editDream = []
+    $scope.updateDream = {}
     $http.get(apiUrl + `/${user}/${id}`).then(res => {
-      console.log(res.data);
+      // console.log(res.data);
       $scope.updateDream = res.data['0']
+      $scope.updateDream.dream_date = new Date($scope.updateDream.dream_date)
+      console.log($scope.updateDream);
     })
 
-    // $scope.editDream = $scope.updateDream
-    console.log($scope.updateDream);
-
-    // $scope.editDream = []
-    $scope.click = function () {
-      $http.get(`${apiUrl}/${user}/${id}`).then(res => {
-        // $scope.editDream = res.data
-
-      })
-        // console.log('hellooooo');
-    }
-
     $scope.editDream = function (updateDream) {
-      console.log(updateDream);
       $http.put(apiUrl + `/${user}/${id}`, updateDream).then(res => {
       $state.go('menu.allDreams')
       }).catch(err => {
