9e1cf4f631f6af9d42c6ace72aea1b7ee26b1df7
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-07 11:10:02 -0700

    post working; working on making toggle elements default to false

--- www/js/controllers/post.js
+++ www/js/controllers/post.js
@@ -1,12 +1,14 @@
 angular.module('app.post', [])
 
+.controller('newFormCtrl', ['$scope', '$stateParams', '$http',
+  function ($scope, $stateParams, $http) {
+    const apiUrl = 'https://dream-frog.herokuapp.com'
 
-.controller ('postCtrl', ['$scope', '$stateParams',
-function ($scope, $stateParams) {
-
-}])
-
-.controller('newFormCtrl', ['$scope', '$stateParams',
-function ($scope, $stateParams) {
-
-}])
+    $scope.createDream = function (newDream) {
+      console.log('button');
+      $http.post(`${apiUrl}/andromeda111/`, newDream).then(result => {
+        console.log(newDream, result.data)
+      })
+    }
+  }
+])
