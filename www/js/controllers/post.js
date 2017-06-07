51ba3c44421fc2ccacee3a157b0d821cda3f7008
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-07 15:33:48 -0700

    accidentally in master; newForm url dynamic

--- www/js/controllers/post.js
+++ www/js/controllers/post.js
@@ -1,16 +1,19 @@
 angular.module('app.post', [])
 
-.controller('newFormCtrl', ['$scope', '$stateParams', '$http', '$state',
-  function ($scope, $stateParams, $http, $state) {
+.controller('newFormCtrl', ['$scope', '$stateParams', '$http', '$state', '$ionicUser',
+  function ($scope, $stateParams, $http, $state, $ionicUser) {
+    const user = $ionicUser.details.username
     const apiUrl = 'https://dream-frog.herokuapp.com'
     $scope.newDream = {}
+    $scope.newDream.dream_date = new Date()
+
     $scope.newDream.lucidity = 0
     $scope.newDream.nightmare = false
     $scope.newDream.recurring = false
+    console.log(user)
 
     $scope.createDream = function (newDream) {
-      console.log('button')
-      $http.post(`${apiUrl}/missalyss/`, newDream).then(result => {
+      $http.post(`${apiUrl}/${user}/`, newDream).then(result => {
         $state.go('menu.allDreams')
       })
     }
