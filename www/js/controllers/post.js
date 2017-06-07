ccd04c60da6895eb90fec976cb0cffd9ca7521fa
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-07 11:56:35 -0700

    all posting problems working, redirects to all dreams

--- www/js/controllers/post.js
+++ www/js/controllers/post.js
@@ -1,13 +1,17 @@
 angular.module('app.post', [])
 
-.controller('newFormCtrl', ['$scope', '$stateParams', '$http',
-  function ($scope, $stateParams, $http) {
+.controller('newFormCtrl', ['$scope', '$stateParams', '$http', '$state',
+  function ($scope, $stateParams, $http, $state) {
     const apiUrl = 'https://dream-frog.herokuapp.com'
+    $scope.newDream = {}
+    $scope.newDream.lucidity = 0
+    $scope.newDream.nightmare = false
+    $scope.newDream.recurring = false
 
     $scope.createDream = function (newDream) {
-      console.log('button');
-      $http.post(`${apiUrl}/andromeda111/`, newDream).then(result => {
-        console.log(newDream, result.data)
+      console.log('button')
+      $http.post(`${apiUrl}/missalyss/`, newDream).then(result => {
+        $state.go('menu.allDreams')
       })
     }
   }
