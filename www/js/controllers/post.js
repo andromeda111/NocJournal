d9c9c00be39d9cdfcab987e2cee07a800bed5312
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-06-08 10:10:59 -0700

    Added speech to text, more dream string manipulation

--- www/js/controllers/post.js
+++ www/js/controllers/post.js
@@ -10,12 +10,56 @@ angular.module('app.post', [])
     $scope.newDream.lucidity = 0
     $scope.newDream.nightmare = false
     $scope.newDream.recurring = false
-    console.log(user)
 
     $scope.createDream = function (newDream) {
       $http.post(`${apiUrl}/${user}/`, newDream).then(result => {
         $state.go('menu.allDreams')
       })
     }
+
+    // Speech to text variables and fn
+    $scope.dreamStringArray = []
+    $scope.dreamString = ''
+
+    $scope.record = function () {
+      var recognition = new webkitSpeechRecognition() // To Computer
+      // var recognition = new SpeechRecognition() // To Device, iPhone?
+      recognition.lang = '' // Defaults to device lang setting
+
+      recognition.onresult = function(event) {
+        if (event.results.length > 0) {
+          let str = event.results[0][0].transcript
+
+          // Add profanity back in  ^_____^
+          str = str.replace("f******", "fucking")
+          str = str.replace("f***", "fuck")
+          str = str.replace("b*******", "bitching")
+          str = str.replace("b****", "bitch")
+          str = str.replace("s*******", "shitting")
+          str = str.replace("s***", "shit")
+          str = str.replace("a******", "asshole")
+
+          let firstLetter = str.substring(0, 1).toUpperCase()
+          let everythingElse = str.substring(1)
+
+          $scope.recognizedText = firstLetter + everythingElse
+          const a = $scope.recognizedText
+
+          if (a.substring(a.length - 1) !== ".") {
+            $scope.recognizedText += "."
+          }
+
+          $scope.dreamStringArray.push($scope.recognizedText)
+          $scope.dreamString = $scope.dreamStringArray.join(" ")
+
+          $scope.newDream.body = $scope.dreamString
+
+          $scope.$apply()
+        }
+      }
+
+      recognition.start()
+    }
+
   }
 ])
