164e27b663757b1ad1209581badba8e631d22e9e
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-07 19:22:38 -0700

    got some stats going

--- www/js/controllers/user.js
+++ www/js/controllers/user.js
@@ -4,13 +4,26 @@ angular.module('app.user', [])
   function ($scope, $stateParams, $http, $state, $ionicUser) {
     const user = $ionicUser.details.username
     const apiUrl = 'https://dream-frog.herokuapp.com'
-
+    $scope.all = []
     getStats()
 
     function getStats () {
       $http.get(`${apiUrl}/${user}/`).then(result => {
-        console.log(result.data)
+        $scope.all = result.data
+        $scope.all.nightmareCounter = 0
+        $scope.all.recurringCounter = 0
+
+        $scope.all.emotionCounter = {confused: 5, excited: 0 }
+        // console.log($scope.all.emotionCounter.confused)
+// consider turning the counter into a map, ++ to each item in the array 
+        $scope.all.forEach(dream => {
+          dream.nightmare ? $scope.all.nightmareCounter++ : $scope.all.nightmareCounter
+          dream.recurring ? $scope.all.recurringCounter++ : $scope.all.recurringCounter
+          // dream.emotion ? $scope.all.emotionCounter.confused ++ : $scope.all.emotionCounter.confused
+        })
       })
     }
   }
 ])
+
+// silly, confused, anxious, afraid, frustrated, angry, sad, indifferent, aroused}
