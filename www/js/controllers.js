8e1f0d6b833e037bd553fdaa5f03996d617bc9bc
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-08 07:50:04 -0700

    pluralize some user stuff, working on symbol html and controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,6 +4,25 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for these functions
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 
+.controller('symbolismCtrl', ['$scope', '$http', '$stateParams',
+function ($scope, $http, $stateParams) {
+  const symbolApi = 'https://dream-symbolism-api.herokuapp.com'
+  $scope.symbols = []
+
+  getAllSymbols()
+
+  function getAllSymbols () {
+    $http.get(symbolApi).then(allSymbols => {
+      $scope.symbols = allSymbols.data
+    })
+  }
+
+  // $scope.getOneSymbol = function () {
+  //   $http.get(`${symbolApi}/`)
+  // }
+
+}])
+
 //////////////////// RESOURCES AND USER STUFF BELOW ///////////////////////////////////////
 
 .controller('calendarCtrl', ['$scope', '$stateParams',
@@ -11,10 +30,6 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('symbolismCtrl', ['$scope', '$stateParams',
-function ($scope, $stateParams) {
-
-}])
 
 .controller('lucidCtrl', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
