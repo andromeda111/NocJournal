856f8dcc85c25361d26a222379ca66c5883ae0c3
John <jmseymour111@gmail.com>
2017-06-08 12:49:04 -0700

    working

--- www/js/controllers/user.js
+++ www/js/controllers/user.js
@@ -4,25 +4,108 @@ angular.module('app.user', [])
   function ($scope, $stateParams, $http, $state, $ionicUser) {
     const user = $ionicUser.details.username
     const apiUrl = 'https://dream-frog.herokuapp.com'
-    $scope.all = []
-    getStats()
 
-    function getStats () {
-      $http.get(`${apiUrl}/${user}/`).then(result => {
-        $scope.all = result.data
-        $scope.all.nightmareCounter = 0
-        $scope.all.recurringCounter = 0
+    $scope.chartObject = {};
 
-        $scope.all.emotionCounter = {confused: 5, excited: 0 }
-        // console.log($scope.all.emotionCounter.confused)
-// consider turning the counter into a map, ++ to each item in the array 
-        $scope.all.forEach(dream => {
-          dream.nightmare ? $scope.all.nightmareCounter++ : $scope.all.nightmareCounter
-          dream.recurring ? $scope.all.recurringCounter++ : $scope.all.recurringCounter
-          // dream.emotion ? $scope.all.emotionCounter.confused ++ : $scope.all.emotionCounter.confused
-        })
-      })
-    }
+$scope.chartObject.type = "PieChart";
+
+$scope.chartObject.data = {
+    "cols": [
+        { id: "t", label: "Emotions", type: "string" },
+        { id: "s", label: "Occurence", type: "number" }
+    ], "rows": [
+        {
+            c: [
+               { v: "One" },
+               { v: 1 },
+            ]
+        },
+        {
+            c: [
+               { v: "Two" },
+               { v: 2 }
+            ]
+        },
+        {
+            c: [
+               { v: "Three" },
+               { v: 3 },
+            ]
+        },
+        {
+            c: [
+               { v: "Four" },
+               { v: 1 },
+            ]
+        },
+        {
+            c: [
+               { v: "Five" },
+               { v: 1 },
+            ]
+        },
+        {
+            c: [
+               { v: "Six" },
+               { v: 6 }
+            ]
+        },
+        {
+            c: [
+               { v: "Seven" },
+               { v: 5 },
+            ]
+        },
+        {
+            c: [
+               { v: "Eight" },
+               { v: 1 },
+            ]
+        },
+        {
+            c: [
+               { v: "Nine" },
+               { v: 7 },
+            ]
+        },
+        {
+            c: [
+               { v: "Ten" },
+               { v: 5 },
+            ]
+        }
+    ]
+};
+
+$scope.chartObject.options = {
+    'legend':'bottom',
+    // 'title':'Dream History Analysis',
+    'is3D':false,
+    'sliceVisibilityThreshold': 0.1,
+    'width':350,
+    'height':300,
+    'colors': ['#182033', '#273453', '#34466F', '#41578B', '#4E69A6', '#667FB7', '#8296C4', '#9EADD1', '#BAC5DE', '#D5DCEB']
+};
+
+//     $scope.all = []
+//     getStats()
+//
+//     function getStats () {
+//       $http.get(`${apiUrl}/${user}/`).then(result => {
+//         $scope.all = result.data
+//         $scope.all.nightmareCounter = 0
+//         $scope.all.recurringCounter = 0
+//
+//         $scope.all.emotionCounter = {confused: 5, excited: 0 }
+//         // console.log($scope.all.emotionCounter.confused)
+// // consider turning the counter into a map, ++ to each item in the array
+//         $scope.all.forEach(dream => {
+//           dream.nightmare ? $scope.all.nightmareCounter++ : $scope.all.nightmareCounter
+//           dream.recurring ? $scope.all.recurringCounter++ : $scope.all.recurringCounter
+//           // dream.emotion ? $scope.all.emotionCounter.confused ++ : $scope.all.emotionCounter.confused
+//         })
+//       })
+//     }
   }
 ])
 
