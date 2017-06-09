212acb1450d9e69313b5523c7bb00e635582b8bb
John <jmseymour111@gmail.com>
2017-06-09 11:47:42 -0700

    updated App ID

--- www/js/ionic.cloud.init.js
+++ www/js/ionic.cloud.init.js
@@ -3,7 +3,7 @@ angular.module('ionic.cloud.init', ['ionic.cloud'])
 .config(function($ionicCloudProvider) {
   $ionicCloudProvider.init({
     "core": {
-      "app_id": "d7df0cff"
+      "app_id": "f0a11376"
     }
   });
 })
